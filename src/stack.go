5dfb133a0dc1f25dd27df540076a01e7a2045eb1
gods-mack <manish-1004@outlook.com>
2021-04-03 02:05:25 +0530

    stack data str in Go

---
+++ src/stack.go
@@ -0,0 +1,54 @@
+package main
+
+import ("fmt")
+
+type node struct {
+	data int
+	next *node
+}
+
+
+type stack struct {
+	top *node
+	head *node
+}
+
+
+func push(x int, stak *stack) {
+
+	if stak.top == nil {
+		stak.top= &node{data:x, next:nil}
+	} else{
+		stak.top =  &node{data:x, next:stak.top}
+
+	}
+
+}
+
+
+func pop(stak *stack) {
+
+	if stak.top == nil {
+		fmt.Println("Stack is empty!")
+		return
+	} else {
+		stak.top = stak.top.next
+	}
+}
+
+
+
+func main() {
+
+	mystack := stack{top:nil, head:nil} 
+	push(10, &mystack)
+	push(15, &mystack)
+	push(16, &mystack)
+	push(23, &mystack)
+	pop(&mystack)
+	push(76, &mystack)
+	pop(&mystack)
+
+	fmt.Println("stack top: ", mystack.top.data)
+
+}
